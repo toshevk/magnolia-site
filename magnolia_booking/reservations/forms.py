619b332a42ba93f5fd47c69b18569cbfce0dc5d7
DESKTOP-KRSTE\toshe <toshev.krste@yahoo.com>
2026-01-28 16:41:14 +0100

    Add BookingWizardView for managing booking flow. Add 3 new forms for BookingWizardView.

--- magnolia_booking/reservations/forms.py
+++ magnolia_booking/reservations/forms.py
@@ -1,39 +1,50 @@
+import datetime
 from django import forms
-from .models import Reservation
+from .models import Reservation, Room
 
 class ReservationForm(forms.ModelForm):
     honeypot = forms.CharField(required=False, widget=forms.HiddenInput)
 
     class Meta:
         model = Reservation
-        fields = ['room', 'guest_name', 'check_in', 'check_out', 'group_size']
-        widgets = {
-            'check_in': forms.DateInput(attrs={'type':'date'}),
-            'check_out': forms.DateInput(attrs={'type':'date'})
-        }
+        fields = ['room',
+                  'check_in',
+                  'check_out',
+                  'group_size',
+                  'guest_name',
+                  'guest_telephone',
+                  'guest_email'
+        ]
 
     def clean_honeypot(self):
         if self.cleaned_data.get('honeypot'):
             raise forms.ValidationError("Bot detected.")
         return self.cleaned_data['honeypot']
 
-class BookingSearchForm(forms.ModelForm):
-    honeypot = forms.CharField(required=False, widget=forms.HiddenInput)
+class TripRequirementsForm(forms.Form):
+    check_in = forms.DateField(widget=forms.DateInput(attrs={"type": "date"}))
+    check_out = forms.DateField(widget=forms.DateInput(attrs={"type": "date"}))
+    group_size = forms.IntegerField(min_value=1, max_value=10)
 
-    check_in = forms.DateField(
-        widget=forms.DateInput(attrs={'type':'date'}),
-        label='Check-in')
+    def clean(self):
+        cleaned = super().clean()
+        if cleaned.get("check_in") and cleaned.get("check_out"):
+            if cleaned["check_in"] < datetime.date.today():
+                raise forms.ValidationError("Earliest you can check-in is today.")
+            if cleaned["check_out"] <= cleaned["check_in"]:
+                raise forms.ValidationError("Check-out must be after check-in.")
+        return cleaned
 
-    check_out = forms.DateField(
-        widget=forms.DateInput(attrs={'type':'date'}),
-        label="Check-out")
+class RoomChoiceForm(forms.Form):
+    room = forms.ModelChoiceField(queryset=Room.objects.none(),
+                                  empty_label='Select a Room')
 
-    group_size = forms.IntegerField(
-        min_value=1,
-        max_value=10,
-        label="Number of Guests")
+    def __init__(self, *args, **kwargs):
+        queryset = kwargs.pop("queryset", Room.objects.none())
+        super().__init__(*args, **kwargs)
+        self.fields["room"].queryset = queryset
 
-    def clean_honeypot(self):
-        if self.cleaned_data.get('honeypot'):
-            raise forms.ValidationError("Bot detected.")
-        return self.cleaned_data['honeypot']
+class GuestDetailsForm(forms.Form):
+    name = forms.CharField(min_length=2, max_length=50)
+    email = forms.EmailField()
+    phone_number = forms.CharField()
--- magnolia_booking/reservations/views.py
+++ magnolia_booking/reservations/views.py
@@ -1,124 +1,146 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse
-from urllib.parse import urlencode
-from .forms import ReservationForm, BookingSearchForm
+from django.http import HttpResponse
+from django.template.context_processors import request
+
+# from django.urls import reverse
+# from urllib.parse import urlencode
+from .forms import ReservationForm, TripRequirementsForm, RoomChoiceForm, GuestDetailsForm
 from .models import Room
+from formtools.wizard.views import SessionWizardView
+
+class BookingWizardView(SessionWizardView):
+    form_list = [("trip_requirements", TripRequirementsForm),
+                 ("room_choice", RoomChoiceForm),
+                 ("guest_details", GuestDetailsForm)
+    ]
+    template_name = "reservations/reservation_form_wizard.html"
+
+    def done(self, form_list, **kwargs):
+        booking_requirements_form = form_list[0]
+        booking_room_form = form_list[1]
+        booking_details_form = form_list[2]
+
+        print(f"{booking_details_form.cleaned_data['name']} made a new reservation!")
+        print(booking_requirements_form.cleaned_data)
+        print(booking_room_form.cleaned_data)
+        print(booking_details_form.cleaned_data)
+
+        return redirect("reservations:reservation_success")
+        # return render(request=self.request,
+        #               template_name="reservations/reservation_success.html",
+        #               context={'form_list': form_list})
 
 
-def booking_search(request):
-    form = BookingSearchForm(request.GET or None)
-    if form.is_valid():
-        check_in = form.cleaned_data['check_in']
-        check_out = form.cleaned_data['check_out']
-        group_size = form.cleaned_data['group_size']
+    def get_form_kwargs(self, step=None):
+        kwargs = super().get_form_kwargs(step)
 
-        base_url = reverse("booking_results")
-        query = urlencode({
-            'check_in': check_in,
-            'check_out': check_out,
-            'group_size': group_size,
-        })
-        return redirect(f"{base_url}?{query}")
+        if step == "room_choice":
+            step1_data = self.get_cleaned_data_for_step("trip_requirements")
 
-    return render(request=request,
-                  template_name="reservations/reservation_form_step1.html",
-                  context={
-                      'form': form,
-                  })
+            if step1_data:
+                group_size = step1_data["group_size"]
+                kwargs["queryset"] = Room.objects.filter(capacity__gte=group_size)
 
-def booking_result(request):
-    room_requirements = {
-        "check_in": request.GET.get("check_in"),
-        "check_out": request.GET.get("check_out"),
-        "group_size": request.GET.get("group_size"),
-    }
+        return kwargs
 
-    rooms = Room.objects.filter(capacity__gte=room_requirements['group_size'])
-    return render(request=request,
-                  template_name="reservations/reservation_form_step2.html",
-                  context={
-                      'room_requirements': room_requirements,
-                      'rooms': rooms,
-                  })
+# def booking_search(request):
+#     form = BookingSearchForm(request.POST or None)
+#     if form.is_valid():
+#         request.session["check_in"] = str(form.cleaned_data["check_in"])
+#         request.session["check_out"] = str(form.cleaned_data["check_out"])
+#         request.session["group_size"] = form.cleaned_data["group_size"]
+#
+#         return redirect("reservations:booking_result")
+#
+#     return render(request=request,
+#                   template_name="reservations/reservation_form_step1.html",
+#                   context={
+#                       'form': form,
+#                   })
+#
+# def booking_result(request):
+#     check_in = request.session.get("check_in")
+#     check_out = request.session.get("check_out")
+#     group_size = request.session.get("group_size")
+#
+#     rooms = Room.objects.filter(capacity__gte=group_size)
+#     return render(request=request,
+#                   template_name="reservations/reservation_form_step2.html",
+#                   context={
+#                       'rooms': rooms,
+#                   })
+#
+# def booking_confirm(request, pk=None):
+#     room = None
+#     if pk is not None:
+#         room = get_object_or_404(Room, pk=pk)
+#
+#     room_requirements = {
+#         "check_in": request.session.get("check_in"),
+#         "check_out": request.session.get("check_out"),
+#         "group_size": request.session.get("group_size"),
+#     }
+#
+#     if request.method == "POST":
+#         form = ReservationForm(request.POST)
+#         if form.is_valid():
+#             reservation = form.save(commit=False)
+#             if room:
+#                 reservation.room = room
+#
+#             form.save()
+#             return redirect('reservations:reservation_success')
+#
+#     else:
+#         if room:
+#             form = ReservationForm(initial={"room": room,
+#                                             "check_in": room_requirements['check_in'],
+#                                             "check_out": room_requirements['check_out'],
+#                                             "group_size": room_requirements['group_size']})
+#         else:
+#             form = ReservationForm()
+#
+#     return render(request=request,
+#                   template_name="reservations/reservation_form_step3.html",
+#                   context={
+#                       'room': room,
+#                       'form': form,
+#                       'room_requirements': room_requirements,
+#                   })
 
-def booking_confirm(request, pk=None):
-    room = None
-    room_requirements = {
-        "check_in": request.GET.get("check_in"),
-        "check_out": request.GET.get("check_out"),
-        "group_size": request.GET.get("group_size"),
-    }
-
-    if pk is not None:
-        room = get_object_or_404(Room, pk=pk)
-
-    if request.method == "POST":
-        form = ReservationForm(request.POST)
-        if form.is_valid():
-            reservation = form.save(commit=False)
-            if room:
-                reservation.room = room
-
-            form.save()
-            return redirect('reservations:reservation_success')
-
-    #
-    #   OTKRIJ JA GRESKATA
-    #   VIDI STO PUSTA POST REQUESTOT DO MODEL FORM
-    #
-    #   MOZNO E INITIAL VREDNOSTITE
-    #
-
-    else:
-        if room:
-            form = ReservationForm(initial={"room": room,
-                                            "check_in": room_requirements['check_in'],
-                                            "check_out": room_requirements['check_out'],
-                                            "group_size": room_requirements['group_size']})
-        else:
-            form = ReservationForm()
-
-    return render(request=request,
-                  template_name="reservations:reservation_step3.html",
-                  context={
-                      'room': room,
-                      'form': form,
-                      'room_requirements': room_requirements,
-                  })
-
-def make_reservation(request, pk=None):
-    room = None
-    room_requirements = {
-        "check_in": request.GET.get("check_in"),
-        "check_out": request.GET.get("check_out"),
-        "group_size": request.GET.get("group_size"),
-    }
-
-    if pk is not None:
-        room = get_object_or_404(Room, pk=pk)
-
-    if request.method == "POST":
-        form = ReservationForm(request.POST)
-        if form.is_valid():
-            reservation = form.save(commit=False)
-            if room:
-                reservation.room = room
-
-            form.save()
-            return redirect('reservations:reservation_success')
-    else:
-        if room:
-            form = ReservationForm(initial={"room": room})
-        else:
-            form = ReservationForm()
-
-    make_reservation_context = {
-        "form": form,
-        "room": room
-    }
-    return render(request=request,
-                  template_name="reservations/reservation_form.html",
-                  context=make_reservation_context)
+# def make_reservation(request, pk=None):
+#     room = None
+#     room_requirements = {
+#         "check_in": request.GET.get("check_in"),
+#         "check_out": request.GET.get("check_out"),
+#         "group_size": request.GET.get("group_size"),
+#     }
+#
+#     if pk is not None:
+#         room = get_object_or_404(Room, pk=pk)
+#
+#     if request.method == "POST":
+#         form = ReservationForm(request.POST)
+#         if form.is_valid():
+#             reservation = form.save(commit=False)
+#             if room:
+#                 reservation.room = room
+#
+#             form.save()
+#             return redirect('reservations:reservation_success')
+#     else:
+#         if room:
+#             form = ReservationForm(initial={"room": room})
+#         else:
+#             form = ReservationForm()
+#
+#     make_reservation_context = {
+#         "form": form,
+#         "room": room
+#     }
+#     return render(request=request,
+#                   template_name="reservations/reservation_form.html",
+#                   context=make_reservation_context)
 
 def reservation_success(request):
     return render(request=request,
