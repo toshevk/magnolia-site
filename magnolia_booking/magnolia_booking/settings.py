d48db6f35c43909eb95f3e62d2b8a64f6294c4e8
DESKTOP-KRSTE\toshe <toshev.krste@yahoo.com>
2026-01-28 16:45:00 +0100

    Add formtools, crispy_forms as modules. Update crispy_forms template pack

--- magnolia_booking/magnolia_booking/settings.py
+++ magnolia_booking/magnolia_booking/settings.py
@@ -39,10 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'formtools',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'core',
     'rooms',
     'reservations',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
