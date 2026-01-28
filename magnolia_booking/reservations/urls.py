0e5fb49cf0e45ceefaf1d42a3d2f7d109df1dfcc
DESKTOP-KRSTE\toshe <toshev.krste@yahoo.com>
2026-01-28 16:43:34 +0100

    Update urls to route BookingWizardView

--- magnolia_booking/reservations/urls.py
+++ magnolia_booking/reservations/urls.py
@@ -4,9 +4,11 @@ from . import views
 app_name = "reservations"
 
 urlpatterns = [
-    path('request_room/search/', views.booking_search, name="booking_search"),
-    path('request_room/results/', views.booking_result, name="booking_result"),
-    path('reserve/', views.make_reservation, name="make_reservation"),
-    path('reserve/<int:pk>', views.make_reservation, name="make_reservation_select_room"),
+    path('request_reservation/', views.BookingWizardView.as_view(), name="request_reservation"),
+    # path('request_room/search/', views.booking_search, name="booking_search"),
+    # path('request_room/results/', views.booking_result, name="booking_result"),
+    # path('request_room/confirm/', views.booking_confirm, name="booking_confirm"),
+    # path('reserve/', views.make_reservation, name="make_reservation"),
+    # path('reserve/<int:pk>', views.make_reservation, name="make_reservation_select_room"),
     path('success/', views.reservation_success, name="reservation_success"),
 ]
